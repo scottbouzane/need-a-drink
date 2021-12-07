a42fc6b37be6c2139e4900e256ac2ca4c5e50cc1
Scott Bouzane <scottbouzane@Scotts-MacBook-Pro.local>
2021-12-07 09:36:40 -0500

    apicall

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const BASE = 'app2zq4L6guOxQs25'
+  const KEY = 'keyT3pmuKrfPrLAwu'
+  const BASE_URL = 'https://api.airtable.com/v0/${BASE}/brewapp'
+  const config = {
+    headers: {
+      Authorization : 'Bearer ${KEY}'
+    }
+  }
+  useEffect(() => {
+
+  })
+
   return (
     <div className="App">
       <header className="App-header">
