6e4cf17c8506879f40f1d7b8e1687245b05c4370
Scott Bouzane <scottbouzane@Scotts-MacBook-Pro.local>
2021-12-07 23:29:01 -0500

    bugfix

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import { useEffect } from 'react'
 import axios from 'axios'
 
+
+
 function App() {
-  const BASE = 'app2zq4L6guOxQs25'
-  const KEY = 'keyT3pmuKrfPrLAwu'
-  const BASE_URL = 'https://api.airtable.com/v0/${BASE}/brewapp'
+  
+  const BASE_URL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/brewapp`
   const config = {
     headers: {
-      Authorization : 'Bearer ${KEY}'
+    Authorization : `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
     }
   }
   useEffect(() => {
@@ -23,20 +24,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
