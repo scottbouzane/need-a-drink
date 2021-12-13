d96675708d967d41a1b66d386f02c44d3fb92668
Scott Bouzane <scottbouzane@Scotts-MacBook-Pro.local>
2021-12-12 20:36:33 -0500

    form

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react'
+
+export default function Form() {
+  const [name, setName] = useState('')
+  const [contact, setContact] = useState('')
+  const [address, setAddress] = useState('')
+}
